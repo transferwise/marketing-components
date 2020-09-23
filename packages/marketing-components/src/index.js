22f6053e66af9e647c96e29d2ad82999d0789925
Gregoire Linot <gregoire.linot@transferwise.com>
2020-09-23 11:57:01 +0100

    feat: Adding canstar award element

--- packages/marketing-components/src/index.js
+++ packages/marketing-components/src/index.js
@@ -1,3 +1,3 @@
 export { default as VideoModal } from './videomodal';
 export { default as YouTubeVideoModal } from './youtubevideomodal';
-export { FCARegulated, Trustpilot } from './trustelements';
+export { FCARegulated, Trustpilot, CanstarAward } from './trustelements';
