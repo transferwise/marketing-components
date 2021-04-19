baace9c94bcea075a34827bd809c5cbeaafa3484
Karan Thakkar <karan.thakkar@transferwise.com>
2021-04-19 14:09:08 +0100

    feat: Add BrazilianLicenseTrustElement (#68)

--- packages/marketing-components/src/index.js
+++ packages/marketing-components/src/index.js
@@ -5,6 +5,7 @@ export {
   BirlesikOdemeRegulatedTrustElement,
   BNMApprovedTrustElement,
   BrazilianCorrespondentTrustElement,
+  BrazilianLicenseTrustElement,
   CAndEDRegulatedTrustElement,
   CanstarAwardTrustElement,
   CustomersTrustElement,
---
+++ packages/marketing-components/src/trustelements/BrazilianLicenseTrustElement/BrazilianLicenseTrustElement.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Types from 'prop-types';
+
+import TrustElement from '../TrustElement';
+
+const BrazilianLicenseTrustElement = ({ title, linkText, href, useIllustration, ...rest }) => (
+  <TrustElement
+    {...rest}
+    src="https://transferwise.com/public-resources/assets/spot-illustrations/banking.svg"
+    title={title}
+    linkText={linkText}
+    href={href}
+    useIllustration={useIllustration}
+  />
+);
+
+BrazilianLicenseTrustElement.propTypes = {
+  title: Types.oneOfType([Types.element, Types.string]).isRequired,
+  linkText: Types.oneOfType([Types.element, Types.string]).isRequired,
+  href: Types.string.isRequired,
+  useIllustration: Types.bool,
+};
+
+BrazilianLicenseTrustElement.defaultProps = {
+  useIllustration: true,
+};
+
+export default BrazilianLicenseTrustElement;
